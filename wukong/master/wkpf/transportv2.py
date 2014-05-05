2b7b7f9b02db3d053b367ecd7dd1b560ef04677e
Niels Reijers <nielsreijers@gmail.com>
2014-05-05 15:05:32 +0800

    Issue #150 32 bit addresses in the simulator work in progress

--- wukong/master/wkpf/transportv2.py
+++ wukong/master/wkpf/transportv2.py
@@ -19,6 +19,7 @@ from gevent import socket
 from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
 from tinyrpc.transports import ClientTransport
 from tinyrpc import RPCClient
+from transport import NetworkServerAgent
 import mptnUtils
 import ipaddress
 from gevent.lock import RLock
@@ -1088,6 +1089,9 @@ def getAgent():
 
 def getZwaveAgent():
     return RPCAgent.init()
+
+def getNetworkServerAgent():
+    return NetworkServerAgent.init()
 
 def getDeferredQueue():
     return DeferredQueue.init()
